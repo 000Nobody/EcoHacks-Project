5a5f7731e47000b06e2ed82960c38fda5be77a1f
000Nobody <nicolas.cutler.0@gmail.com>
2022-09-03 07:51:58 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+import GoogleMapsTest from "./components/GoogleMapsTest"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Eco Hacks Project
+      </header>
+      <GoogleMapsTest />
+    </div>
+  );
+}
+
+export default App;
