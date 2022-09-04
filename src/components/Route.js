ceb9b51aa9827ba549c1627e8f5fd0258f4b9cb2
banaminion55 <75647907+banaminion55@users.noreply.github.com>
2022-09-03 23:04:56 -0700

    Form now submits and saves input

--- src/components/Route.js
+++ src/components/Route.js
@@ -1,22 +1,33 @@
 import {Autocomplete} from '@react-google-maps/api';
+/*import { findAllByDisplayValue } from '@testing-library/react'; need this??*/
 import React, { useRef } from 'react';
 
 
-export function Route() {
+const Route = () => {
 
-  const startLoc=useRef()
-  const endLoc=useRef()
+  const startLoc=useRef();
+  const endLoc=useRef();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const start = document.getElementById("starting-point").value;
+    const end = document.getElementById("destination").value;
+    alert("Hello! \n Start: " + start + " End: " + end);
+    /*Call function here using start and end maybe*/
+  };
 
   return (
     <div className = "route">
         <h1 className ="route-title">ROUTE</h1>
-        <form id="route-form">
+        <form id="route-form" onSubmit={handleSubmit}>
           <label for="starting-point">Starting Point: </label>
           <Autocomplete><input type="text" id="starting-point" placeholder = "start" ref={startLoc} required></input></Autocomplete>
           <br />
           <br />
           <label for="destination">Destination: </label>
           <Autocomplete><input type="text" id="destination" placeholder="end" ref ={endLoc} required></input></Autocomplete>
+          <br />
+          <button type="submit" id="submit">Submit </button>
         </form>
         <div className = "carbon-info">
             <h1 className = "carbon-footprint-title">Carbon Footprint</h1>
